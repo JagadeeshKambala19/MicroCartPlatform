6a200a5b43981cd565c46f7eaf936c809e9a21ed
Jagadeesh Kambala <jagadeeshbabuaws@gmail.com>
2026-04-28 06:34:02 +0530

    chore(terraform): expose repository urls and arns

---
+++ infra/outputs.tf
@@ -0,0 +1,15 @@
+output "repository_urls" {
+  description = "ECR repository URLs"
+  value = {
+    for repo_name, repo in aws_ecr_repository.repos :
+    repo_name => repo.repository_url
+  }
+}
+
+output "repository_arns" {
+  description = "ECR repository ARNs"
+  value = {
+    for repo_name, repo in aws_ecr_repository.repos :
+    repo_name => repo.arn
+  }
+}
