4845f5a3dd60e876b9774464566c903ab03e70d8
Jagadeesh Kambala <jagadeeshbabuaws@gmail.com>
2026-05-02 20:28:21 +0530

    feat(ecs): configure cloudwatch log group for ecs tasks

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -1,7 +1,31 @@
-########################################
-# ECS Cluster
-########################################
+
 resource "aws_ecs_cluster" "main" {
   name = "${var.project_name}-${var.environment}-cluster"
 }
 
+
+resource "aws_cloudwatch_log_group" "ecs" {
+  name              = "/ecs/${var.project_name}-${var.environment}"
+  retention_in_days = 7
+}
+
+
+resource "aws_security_group" "ecs" {
+  name        = "${var.project_name}-${var.environment}-ecs-sg"
+  description = "ECS tasks security group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = var.container_port
+    to_port     = var.container_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # TEMP – will lock to ALB later
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
