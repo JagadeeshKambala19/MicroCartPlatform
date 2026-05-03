ca7044236c652a65af91b2638b71e86ff0301553
Jagadeesh Kambala <jagadeeshbabuaws@gmail.com>
2026-05-03 09:58:13 +0530

    feat(alb): configure http listener to forward traffic to target group

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -39,3 +39,13 @@ resource "aws_lb_target_group" "app" {
     unhealthy_threshold = 2
   }
 }
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
